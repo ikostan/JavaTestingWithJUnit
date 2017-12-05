9fe3d84ad646f4d93652f6bec743b3b94a36960e
ikostan <superegorius@gmail.com>
2017-12-05 15:06:39 -0800

    Sample Assertions

--- src/Calculator/DivideTest.java
+++ src/Calculator/DivideTest.java
@@ -10,17 +10,11 @@ public class DivideTest {
 	public void test() {
 		
 		Divide divide = new Divide();
+		
 		int result = divide.quotient(12, 2);
-		
 		assertEquals("The test is failed!", 6, result);
-	}
-	
-	@Test
-	public void devideByZero() {
 		
-		Divide divide = new Divide();
-		int result = divide.quotient(12, 0);
-		
-		assertEquals("The test is failed!", 0, result);
+		int result2 = divide.quotient(12, 0);
+		assertEquals("The test is failed!", 0, result2);
 	}
 }
